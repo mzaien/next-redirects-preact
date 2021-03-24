ef932af965440a8d1599d442679cd6a365c1ff42
Abdullah Mzaien <s201540830@kfupm.edu.sa>
2021-03-24 21:31:47 +0300

    Adding new api options

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,47 @@ import { useRouter } from "next/router";
 
 export interface nextRedirectsprop {
   href: string;
-  status?: boolean;
+  asPath?: string;
   fallBack?: string;
+  status?: boolean;
+  shallow?: boolean;
+  query?:
+    | string
+    | number
+    | boolean
+    | readonly string[]
+    | readonly number[]
+    | readonly boolean[]
+    | null
+    | undefined;
 }
 
-export function Redirects({ href, status, fallBack }: nextRedirectsprop) {
+export function Redirects({
+  href,
+  status,
+  fallBack,
+  shallow,
+  asPath,
+  query,
+}: nextRedirectsprop) {
   const router = useRouter();
+  status !== undefined && status === true;
   useEffect(() => {
     status !== undefined
-      ? router.push(
-          status === true ? href : fallBack ? fallBack : router.asPath
-        )
-      : router.push(href);
+      ? (router.prefetch(status === true ? href : `${fallBack}`),
+        router.push(
+          {
+            pathname:
+              status === true ? href : fallBack ? fallBack : router.asPath,
+            query: { state: query },
+          },
+          asPath,
+          { shallow: shallow !== undefined ? shallow : false }
+        ))
+      : (router.prefetch(href),
+        router.push({ pathname: href, query: { state: query } }, asPath, {
+          shallow: shallow !== undefined ? shallow : false,
+        }));
   }, [status]);
   return null;
 }
